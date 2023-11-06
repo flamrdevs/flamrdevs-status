84c586dcfc980fbfb64b09f53dc8c923cc6195a7
flamrdevs <flamrdevs@gmail.com>
2023-11-06 18:58:46 +0700

    chore: add health api

---
+++ src/app/api/health/route.ts
@@ -0,0 +1,15 @@
+export async function GET() {
+	return Response.json(
+		{
+			ok: true,
+		},
+		{
+			status: 200,
+			headers: {
+				"Access-Control-Allow-Origin": "*",
+				"Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+				"Access-Control-Allow-Headers": "Content-Type, Authorization",
+			},
+		}
+	);
+}
