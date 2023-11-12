ba721d5d0d33779172a0ecc45bc410f759f48612
flamrdevs <flamrdevs@gmail.com>
2023-11-12 22:42:05 +0700

    refactor: cache

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,66 @@
+import { kv } from "@vercel/kv";
+
+import dayjs from "dayjs";
+
 import { Button } from "~/components/core/index.ts";
 
 import { Item } from "~/app/components.tsx";
+import type { Element } from "~/app/types.ts";
 
-import { getData } from "./get-data.ts";
+const getData = async () => {
+	const length = 45;
+
+	const startDayjs = dayjs().subtract(length, "day").startOf("day");
+	const startTime = startDayjs.toDate().getTime();
+
+	const FORMAT = "DD-MM-YYYY";
+
+	const days = Array.from({ length }).map((_, i) =>
+		startDayjs
+			.clone()
+			.add(i + 1, "day")
+			.format(FORMAT)
+	);
+
+	const keys = await kv.keys("*");
+
+	const data: {
+		name: string;
+		series: {
+			date: string;
+			elements: Element[];
+		}[];
+	}[] = [];
+
+	for await (const key of keys) {
+		const elements = await kv.lrange<Element>(key, 0, -1);
+
+		const preseries = days.reduce((object, key) => {
+			object[key] = [];
+			return object;
+		}, {} as { [key: string]: Element[] });
+
+		for (const element of elements) {
+			const [time] = element;
+			if (time >= startTime) {
+				const key = dayjs(time).format(FORMAT);
+				if (key in preseries) preseries[key].push(element);
+			}
+		}
+
+		data.push({
+			name: key,
+			series: Object.entries(preseries).map(([key, value]) => ({
+				date: key,
+				elements: value,
+			})),
+		});
+	}
+
+	return data;
+};
+
+export const revalidate = 900;
 
 export default async function Index() {
 	const data = await getData();
